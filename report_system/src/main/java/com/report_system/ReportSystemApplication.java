e3ce5def81e2e506df00b2fce308c1eab57a629d
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-01-30 20:48:26 +0200

    added project

---
+++ report_system/src/main/java/com/report_system/ReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.report_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSystemApplication.class, args);
+	}
+
+}
---
+++ report_system/src/test/java/com/report_system/ReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
