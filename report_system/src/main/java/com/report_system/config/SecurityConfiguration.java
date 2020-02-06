164f8cfb681fabaff0fca7c6ca3306bc8311942d
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-02-06 22:09:53 +0200

    did some html and styling work

--- report_system/src/main/java/com/report_system/config/SecurityConfiguration.java
+++ report_system/src/main/java/com/report_system/config/SecurityConfiguration.java
@@ -9,11 +9,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/resources/**").permitAll().anyRequest().authenticated().and().formLogin()
-                .loginPage("/login").permitAll().and().logout().permitAll();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated().and().formLogin()
+                .loginPage("/login").permitAll().and().logout().permitAll();
     }
 }
