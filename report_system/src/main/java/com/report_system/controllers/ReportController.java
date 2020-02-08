b43d334b53d27149e8735aeb610a5be897b85bcc
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-02-09 00:18:25 +0200

    made reports list page

--- report_system/src/main/java/com/report_system/controllers/ReportController.java
+++ report_system/src/main/java/com/report_system/controllers/ReportController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReportController {
     @RequestMapping("/report")
-    public String showLogin() {
+    public String showCreateReport() {
         return "report";
+    }
+
+    @RequestMapping("/reports")
+    public String showReports() {
+        return "reports-list";
     }
 }
