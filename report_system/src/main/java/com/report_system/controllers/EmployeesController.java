a3098669027440ec2fc2d2ec58ed61549ebd3a99
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-02-11 12:29:26 +0200

    improved reports pages

--- report_system/src/main/java/com/report_system/controllers/EmployeesController.java
+++ report_system/src/main/java/com/report_system/controllers/EmployeesController.java
@@ -9,4 +9,9 @@ public class EmployeesController {
     public String showEmployeesManagement() {
         return "manage-employees";
     }
+
+    @RequestMapping("/employee/reports")
+    public String showEmployeeReports() {
+        return "employee-reports";
+    }
 }
