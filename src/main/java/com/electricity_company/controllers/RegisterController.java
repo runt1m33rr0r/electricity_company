3bdb87237b11cf0ee46e44dc499399aaf4f06f54
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-04-10 01:17:06 +0300

    fixed login and register pages

--- src/main/java/com/electricity_company/controllers/RegisterController.java
+++ src/main/java/com/electricity_company/controllers/RegisterController.java
@@ -1,17 +1,19 @@
 package com.electricity_company.controllers;
 
+import com.electricity_company.entities.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
     @RequestMapping("/clients/register")
-    public String showClientRegister() {
+    public String showClientRegister(User user) {
         return "register-client";
     }
 
     @RequestMapping("/employees/register")
-    public String showEmployeeRegister() {
+    public String showEmployeeRegister(User user) {
         return "register-employee";
     }
 }
